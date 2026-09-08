a5d2acb3a1c49b38e24716a4e505d800516ecd99
mmnikonovamorozova-boop <mmnikonovamorozova@gmail.com>
2026-09-08 18:41:50 +0400

    Update app.py

--- app.py
+++ app.py
@@ -117,16 +117,18 @@ else:
         st.rerun()
 
     список_страниц = [
-        st.Page("pages/1_vhodnoy_kontrol.py", title="1. Входной контроль"),
-        st.Page("pages/2_raschet_umk.py", title="2. Расчет УМК"),
-        st.Page("pages/3_tech_cards.py", title="3. Техкарты"),
-        st.Page("pages/8_lyuft_vzd.py", title="4. Люфт ВЗД")
-    ]
-    список_страниц.extend([
-        st.Page("pages/4_matrix_and_lnd.py", title="5. Матрица и ЛНД"),
-        st.Page("pages/5_kontrol_rastvora.py", title="6. Контроль раствора"),
-        st.Page("pages/6_baza_znaniy.py", title="7. База знаний"),
-        st.Page("pages/7_prognoz_traektorii.py", title="8. Прогноз траектории")
-    ])
-    
-    st.navigation(список_страниц).run()
+    st.Page("pages/1_vhodnoy_kontrol.py", title="1. Входной контроль"),
+    st.Page("pages/2_raschet_umk.py", title="2. Расчет УМК"),
+    st.Page("pages/3_tech_cards.py", title="3. Техкарты"),
+    st.Page("pages/8_lyuft_vzd.py", title="4. Люфт ВЗД")
+]
+
+список_страниц.extend([
+    st.Page("pages/4_matrix_and_lnd.py", title="5. Матрица и ЛНД"),
+    st.Page("pages/5_kontrol_rastvora.py", title="6. Контроль раствора"),
+    st.Page("pages/6_baza_znaniy.py", title="7. База знаний"),
+    st.Page("pages/7_prognoz_traektorii.py", title="8. Прогноз траектории"),
+    st.Page("pages/1_sbor_knbk.py", title="9. Сборка КНБК")
+])
+
+st.navigation(список_страниц).run()
