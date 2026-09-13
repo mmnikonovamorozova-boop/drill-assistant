535ef2a4157fe396a66f3b5d99b814569df1eb7e
mmnikonovamorozova-boop <mmnikonovamorozova@gmail.com>
2026-09-13 19:26:45 +0400

    Update 9_sbor_knbk.py

--- pages/9_sbor_knbk.py
+++ pages/9_sbor_knbk.py
@@ -529,6 +529,7 @@ st.caption(context_banner)
 
 # 🛡️ ИНИЦИАЛИЗАЦИЯ ИИ-ЯДРА СМК ПО СТО ИНТИ
 calculated_total_risk = 5.0
+dynamic_stop_threshold = 80.0
 risk_points = 5.0
 base_stop_threshold = 80.0
 joints_rows_html = ""
